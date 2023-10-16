1a77ee6b2c954cd429d85c5b4cabd371770a80b7
HubertP1 <126475707+HubertP1@users.noreply.github.com>
2023-10-16 19:01:56 +0200

    Add u mnie działa

--- NBD/src/main/java/org/example/Main.java
+++ NBD/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import model.Student;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+        EntityManager em = emf.createEntityManager();
+        Student student = new Student();
+        student.setName("Kowalski");
+        em.getTransaction().begin();
+        em.persist(student);
+        em.getTransaction().commit();
     }
 }
