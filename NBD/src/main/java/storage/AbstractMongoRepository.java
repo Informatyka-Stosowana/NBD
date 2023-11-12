96938c5862c8b37c002b9ef95d31468a2641e6b0
DominikKwiecien0 <242448@edu.p.lodz.pl>
2023-11-12 23:52:44 +0100

    Update AbstractMongoRepository

--- NBD/src/main/java/storage/AbstractMongoRepository.java
+++ NBD/src/main/java/storage/AbstractMongoRepository.java
@@ -6,11 +6,26 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.Conventions;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import java.util.List;
 
 public abstract class AbstractMongoRepository implements AutoCloseable {
-    private final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
+//    private final ConnectionString connectionString =
+//            new ConnectionString("mongodb://localhost:27017,localhost:27018,localhost:27019/?replicaSet=replica_set_single");
+    //W zasadzie chyba może być to ze względu na modyfikację pliku hosts
+    private final ConnectionString connectionString =
+            new ConnectionString("mongodb://localhost:27017/?replicaSet=replica_set_single");
     private final MongoCredential mongoCredential = MongoCredential.createCredential("admin", "admin",
             "adminpassword".toCharArray());
+    private final CodecRegistry pojoCodecRegistry =
+            CodecRegistries.fromProviders(PojoCodecProvider.builder()
+                    .automatic(true)
+                    .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
+                    .build());
 
     private MongoClient mongoClient;
     private MongoDatabase mongoDatabase;
@@ -19,6 +34,10 @@ public abstract class AbstractMongoRepository implements AutoCloseable {
         MongoClientSettings settings = MongoClientSettings.builder()
                 .credential(mongoCredential)
                 .applyConnectionString(connectionString)
+                .codecRegistry(CodecRegistries.fromRegistries(
+                        MongoClientSettings.getDefaultCodecRegistry(),
+                        pojoCodecRegistry
+                ))
                 .build();
 
         mongoClient = (MongoClient) MongoClients.create(settings);
