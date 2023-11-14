0038aa7fb6e1c3acda7938f0d34c45a1bb403f61
pietrash <126475707+pietrash@users.noreply.github.com>
2023-11-14 18:52:24 +0100

    Begin rentRepo

--- NBD/src/main/java/model/Rent.java
+++ NBD/src/main/java/model/Rent.java
@@ -1,42 +1,46 @@
-//package model;
-//
-//public class Rent {
-//    private int id;
-//    private double price;
-//    private boolean archive = false;
-//    private Client client;
-//    private Vehicle vehicle;
-//
-//    public Rent(int id, Client client, Vehicle vehicle) {
-//        this.id = id;
-//        this.price = vehicle.getPrice();
-//        this.client = client;
-//        this.vehicle = vehicle;
-//    }
-//
-//    public Rent() {}
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public double getPrice() {
-//        return price;
-//    }
-//
-//    public Client getClient() {
-//        return client;
-//    }
-//
-//    public Vehicle getVehicle() {
-//        return vehicle;
-//    }
-//
-//    public boolean isArchive() {
-//        return archive;
-//    }
-//
-//    public void setArchive(boolean archive) {
-//        this.archive = archive;
-//    }
-//}
+package model;
+
+import ch.qos.logback.core.net.server.Client;
+
+public class Rent {
+    private int id;
+    private double price;
+    private boolean archive;
+    private Client client;
+    private Vehicle vehicle;
+
+    public Rent(int id, Client client, Vehicle vehicle) {
+        this.id = id;
+        this.price = vehicle.getPrice();
+        this.archive = false;
+        this.client = client;
+        this.vehicle = vehicle;
+    }
+
+    public Rent() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public boolean isArchive() {
+        return archive;
+    }
+
+    public void setArchive(boolean archive) {
+        this.archive = archive;
+    }
+}
--- NBD/src/main/java/storage/RentRepository.java
+++ NBD/src/main/java/storage/RentRepository.java
@@ -1,6 +1,78 @@
 package storage;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import model.Rent;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+
 public class RentRepository extends AbstractMongoRepository {
+
+    public RentRepository() {
+        initDbConnection();
+        if (!getMongoDatabase().listCollectionNames().into(new ArrayList()).contains("rents")) {
+            initCollection();
+        }
+    }
+
+    public void initCollection() {
+//        ValidationOptions validationOptions = new ValidationOptions().validator(
+//                Document.parse("""
+//                        {
+//                            $jsonSchema:{
+//                                "bsonType": "object",
+//                                "properties": {
+//                                    "rented": {
+//                                        "bsonType": "int",
+//                                        "minimum" : 0,
+//                                        "maximum" : 1
+//                                        "description": "must be 1 for rented and 0 for available"
+//                                    }
+//                                }
+//                            }
+//                        }
+//                        """));
+//        CreateCollectionOptions createCollectionOptions =
+//                new CreateCollectionOptions().validationOptions(validationOptions);
+//        getMongoDatabase().createCollection("rents", createCollectionOptions);
+        getMongoDatabase().createCollection("rents");
+    }
+
+    public Rent getRent(String id) {
+        MongoCollection<Rent> rentCollection = getMongoDatabase().getCollection("rents", Rent.class);
+        Bson filter = Filters.and(
+                Filters.eq("_id", id)
+        );
+        ArrayList<Rent> rentArrayList = rentCollection.find(filter).into(new ArrayList<>());
+        if (rentArrayList.isEmpty()) return null;
+        return rentArrayList.get(0);
+    }
+
+    public void addRent(Rent rent) {
+        // TODO implement toMongoDb in rent
+//        MongoCollection<Rent> rentCollection = getMongoDatabase().getCollection("rents", Rent.class);
+//        rentCollection.insertOne(vehicle);
+    }
+
+    public boolean updateRent(String id, String fieldToUpdate, String value) {
+        MongoCollection<Rent> rentCollection = getMongoDatabase().getCollection("rents", Rent.class);
+        if (rentCollection.countDocuments() == 0) return false;
+
+        Bson filter = Filters.eq("_id", id);
+        Bson setUpdate = Updates.set(fieldToUpdate, value);
+        rentCollection.updateOne(filter, setUpdate);
+
+        return true;
+    }
+
+    public void removeRent(String id) {
+        MongoCollection<Rent> rentCollection = getMongoDatabase().getCollection("rents", Rent.class);
+
+        Bson filter = Filters.eq("_id", id);
+        rentCollection.findOneAndDelete(filter);
+    }
 
     @Override
     public void close() throws Exception {
